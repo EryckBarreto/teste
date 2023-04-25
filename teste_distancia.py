e0bf48d33ad61b210293a4bb6d4584c4dd673afa
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-25 14:23:13 -0300

    Resolução do problema distância

---
+++ teste_distancia.py
@@ -0,0 +1,12 @@
+# Esse problema pode ser resolvido usando a fórmula de distância, o que me fez voltar
+# ao ensino médio nas aulas de física.
+
+# d = v*t, onde d é a distância 
+# d = distância percorrida
+# v = velocidade
+# t = tempo
+
+# No entanto não creio ser necessário nenhum tipo de conta ou fórmula
+# pois o enunciado pergunta em relação ao ponto de encontro, quem estará
+# mais perto da cidade de Ribeirão Preto. Logo, no ponto de encontro
+# os dois estarão à mesma distância da cidade. 
